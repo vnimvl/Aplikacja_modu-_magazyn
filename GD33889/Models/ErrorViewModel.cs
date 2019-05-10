ebba3f6bde017c7ead3d568742b22ec210a18702
vnimvl <rsobczak96@gmail.com>
2019-05-10 17:33:41 +0200

    Dodaj pliki projektów.

---
+++ GD33889/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace GD33889.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
